b39fb7a4103dc152cdee875a60cd653fd0714146
KrajziXXX <thekrajzixxgaming@gmail.com>
2026-02-11 20:32:41 +0100

    insertcabins

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -9,6 +9,18 @@ export async function getCabins() {
   return cabins;
 }
 
+export async function createCabin(newCabin) {
+  const { data, error } = await supabase
+    .from("cabins")
+    .insert([newCabin])
+    .select();
+  if (error) {
+    console.error(error);
+    throw new Error("Failed to create cabin");
+  }
+  return data;
+}
+
 export async function deleteCabin(id) {
   const { data, error } = await supabase.from("cabins").delete().eq("id", id);
   if (error) {
